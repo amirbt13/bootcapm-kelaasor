0559ebc1cbd7418ba2576a314bda1756be7dc06b
Amir Basirati <amirbasirati13@gmail.com>
2023-07-16 13:29:51 +0330

    list click update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const courseItem = document.querySelectorAll(".courses_list_item_main");
+
 // const submitBtn = document.querySelectorAll(".submit")
 courseItem.forEach((item) => {
   item.addEventListener("click", (e) => {
@@ -14,11 +15,14 @@ courseItem.forEach((item) => {
   });
 });
 
-// submitBtn.forEach(item => {
-//     item.addEventListener("click", async()=> {
-//         const res = await fetch("", {
-//             method: "POST",
-//             body
-//         })
-//     })
-// })
+// const show = (e) => {
+//   const detailDiv = e.target.nextElementSibling;
+//   const arrow = e.target.children[0];
+//   if (!detailDiv.classList.contains("detail_show")) {
+//     detailDiv.classList.add("detail_show");
+//     arrow.classList.add("arrow_up");
+//   } else {
+//     detailDiv.classList.remove("detail_show");
+//     arrow.classList.remove("arrow_up");
+//   }
+// };
