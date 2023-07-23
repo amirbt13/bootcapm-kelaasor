35ad47b53fab9c009996586f7a1649ef4561f305
Amir Basirati <amirbasirati13@gmail.com>
2023-07-23 13:12:01 +0330

    form discount field added

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const courseItem = document.querySelectorAll(".courses_list_item_main");
 
 // const submitBtn = document.querySelectorAll(".submit")
+const show = (e) => {
+  const detailDiv = e.target.nextElementSibling;
+  const arrow = e.target.children[0];
+  if (!detailDiv.classList.contains("detail_show")) {
+    detailDiv.classList.add("detail_show");
+    arrow.classList.add("arrow_up");
+  } else {
+    detailDiv.classList.remove("detail_show");
+    arrow.classList.remove("arrow_up");
+  }
+};
 courseItem.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    const detailDiv = e.target.nextElementSibling;
-    const arrow = e.target.children[0];
-    if (!detailDiv.classList.contains("detail_show")) {
-      detailDiv.classList.add("detail_show");
-      arrow.classList.add("arrow_up");
-    } else {
-      detailDiv.classList.remove("detail_show");
-      arrow.classList.remove("arrow_up");
-    }
-  });
+  item.addEventListener("click", show, { capture: true });
 });
 
 // const show = (e) => {
